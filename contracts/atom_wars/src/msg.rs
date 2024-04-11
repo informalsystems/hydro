f90cccff9cb5426b9112bbe4eca48dcb106619f7
Philip Offtermatt <p.offtermatt@gmail.com>
2024-04-11 12:56:10 +0200

    Add tranches

--- contracts/atom_wars/src/msg.rs
+++ contracts/atom_wars/src/msg.rs
@@ -2,11 +2,15 @@ use cosmwasm_std::Uint128;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use crate::state::Tranche;
+
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     pub denom: String,
     pub round_length: u64,
     pub total_pool: Uint128,
+    pub tranches: Vec<Tranche>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -14,8 +18,8 @@ pub struct InstantiateMsg {
 pub enum ExecuteMsg {
     LockTokens { lock_duration: u64 },
     UnlockTokens {},
-    CreateProposal { covenant_params: String },
-    Vote { proposal_id: u64 },
+    CreateProposal { tranche_id: u64, covenant_params: String },
+    Vote { tranche_id: u64, proposal_id: u64 },
     EndRound {},
     // ExecuteProposal { proposal_id: u64 },
 }
