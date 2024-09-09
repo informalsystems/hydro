eafdade0b39225947a5321c8ef96df080cf27a92
Philip Offtermatt <57488781+p-offtermatt@users.noreply.github.com>
2024-09-09 14:39:21 +0200

    Change valset store init to not require the previous round initialized (#95)
    * Make validator store initialization also initialize uninitialized rounds until an initialized one is found
    
    * Adjust ICQ result handler to use correct initialization method

--- contracts/hydro/src/lsm_integration.rs
+++ contracts/hydro/src/lsm_integration.rs
@@ -251,13 +251,43 @@ pub fn is_validator_store_initialized(storage: &dyn Storage, round_id: u64) -> b
         .unwrap_or(false)
 }
 
-// If the store for this round has not been initialized yet, initialize_validator_store copies the information from the last round
+// Initializes the validator store for the given round.
+// It will try to initialize the store by copying information from the last round.
+// If that round has not been initialized yet, it will try to initialize *that* round
+// from the round before it, and so on.
+// If the validator store for the first round has not been initialized yet, it will return an error.
+// (This should not happen, because the contract initializes with the first round set to initialized)
+pub fn initialize_validator_store(storage: &mut dyn Storage, round_id: u64) -> StdResult<()> {
+    // go back in time until we find a round that has been initialized
+    let mut current_round = round_id;
+    while !is_validator_store_initialized(storage, current_round) {
+        if current_round == 0 {
+            return Err(StdError::generic_err(
+                "Cannot initialize store for the first round because it has not been initialized yet",
+            ));
+        }
+        current_round -= 1;
+    }
+
+    // go forward and initialize the validator stores
+    while current_round < round_id {
+        current_round += 1;
+        initialize_validator_store_helper(storage, current_round)?;
+    }
+
+    Ok(())
+}
+
+// If the store for this round has not been initialized yet, initialize_validator_store_helper copies the information from the last round
 // to seed the store. This is only done starting in the second round.
 // Explicitly, it initializes the VALIDATORS_INFO and the VALIDATORS_PER_ROUND
 // for this round by copying the information from the previous round.
 // If the store of the previous round has not been initialized yet, it returns an error.
 // If the store for this round has already been initialized, or the round_id is for the first round, this function does nothing.
-pub fn initialize_validator_store(storage: &mut dyn Storage, round_id: u64) -> StdResult<()> {
+pub fn initialize_validator_store_helper(
+    storage: &mut dyn Storage,
+    round_id: u64,
+) -> StdResult<()> {
     if round_id == 0 || is_validator_store_initialized(storage, round_id) {
         return Ok(());
     }
