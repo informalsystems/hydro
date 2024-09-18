0882826ae2a87592fdf94e04784eeb4430bc74e0
Philip Offtermatt <p.offtermatt@gmail.com>
2024-09-18 10:20:24 +0200

    Modify script to point to mainnet

--- tools/script.sh
+++ tools/script.sh
@@ -1,9 +1,9 @@
 RELAYER_REPO_PATH=../../neutron-query-relayer
 
 # populate the config for the ICQ relayer and the ICQ query creation
-export NEUTRON_CHAIN_ID="pion-1"
-export RELAYER_NEUTRON_CHAIN_RPC_ADDR=https://rpc-falcron.pion-1.ntrn.tech:443  
-export RELAYER_NEUTRON_CHAIN_REST_ADDR=https://rest-falcron.pion-1.ntrn.tech  
+export NEUTRON_CHAIN_ID="neutron-1"
+export RELAYER_NEUTRON_CHAIN_RPC_ADDR=https://neutron-rpc.publicnode.com:443
+export RELAYER_NEUTRON_CHAIN_REST_ADDR=https://neutron-rest.publicnode.com
 export RELAYER_NEUTRON_CHAIN_HOME_DIR=$HOME/.neutrond
 export RELAYER_NEUTRON_CHAIN_SIGN_KEY_NAME=money
 export RELAYER_NEUTRON_CHAIN_KEYRING_BACKEND=test
@@ -12,12 +12,12 @@ export RELAYER_NEUTRON_CHAIN_GAS_PRICES=0.0055untrn
 export RELAYER_NEUTRON_CHAIN_GAS_PRICE_MULTIPLIER=2  
 export RELAYER_NEUTRON_CHAIN_MAX_GAS_PRICE=0.011  
 export RELAYER_NEUTRON_CHAIN_GAS_ADJUSTMENT=1.5  
-export RELAYER_NEUTRON_CHAIN_CONNECTION_ID=connection-42  
+export RELAYER_NEUTRON_CHAIN_CONNECTION_ID=connection-0
 export RELAYER_NEUTRON_CHAIN_OUTPUT_FORMAT=json  
-export RELAYER_TARGET_CHAIN_RPC_ADDR=https://rpc-rs.cosmos.nodestake.top:443
-export RELAYER_TARGET_CHAIN_API_ADDR=https://cosmos-testnet-api.polkachu.com:443
+export RELAYER_TARGET_CHAIN_RPC_ADDR=https://cosmos-rpc.publicnode.com:443
+export RELAYER_TARGET_CHAIN_API_ADDR=https://api.cosmos.nodestake.org
 # this needs to be the address of the contract
-export RELAYER_REGISTRY_ADDRESSES=neutron15e0r3h6nw4d9yhe2y5kslaq9t35pdk4egm5qd8nytfmzwl9msyssew5339
+export RELAYER_REGISTRY_ADDRESSES=neutron192s005pfsx7j397l4jarhgu8gs2lcgwyuntehp6wundrh8pgkywqgss0tm
 # maximum number of validator queries to submit in a single block.
 # lower this if you get errors about exceeding the max block size
 export BATCH_SIZE=30
@@ -33,7 +33,7 @@ export LOGGER_LEVEL=debug
 #####
 
 # Create the ICQ queries by running the go script in this folder
-go run main.go
+# go run main.go
 
 # Run the relayer
 cd $RELAYER_REPO_PATH
