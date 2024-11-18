02de5d380749d2e4184e87db2908ddbdfbd0f900
Dusan Maksimovic <94966669+dusan-maksimovic@users.noreply.github.com>
2024-11-18 09:39:08 +0100

    add missing types in schema generation (#171)

--- contracts/tribute/src/bin/tribute_schema.rs
+++ contracts/tribute/src/bin/tribute_schema.rs
@@ -4,7 +4,10 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use tribute::msg::{ExecuteMsg, InstantiateMsg};
-use tribute::query::{ConfigResponse, ProposalTributesResponse, QueryMsg};
+use tribute::query::{
+    ConfigResponse, HistoricalTributeClaimsResponse, OutstandingTributeClaimsResponse,
+    ProposalTributesResponse, QueryMsg, RoundTributesResponse,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -18,4 +21,7 @@ fn main() {
 
     export_schema(&schema_for!(ConfigResponse), &out_dir);
     export_schema(&schema_for!(ProposalTributesResponse), &out_dir);
+    export_schema(&schema_for!(HistoricalTributeClaimsResponse), &out_dir);
+    export_schema(&schema_for!(RoundTributesResponse), &out_dir);
+    export_schema(&schema_for!(OutstandingTributeClaimsResponse), &out_dir);
 }
