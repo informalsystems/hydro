066d078db1402fb5b2689f08ec368a107f7dfe1e
Dusan Maksimovic <dusan.maksimovic@ethernal.tech>
2024-12-03 13:25:21 +0100

    - fixed typos - added readme.md - removed github job to run e2e tests since they just instantiate the contracts

--- tools/deployment/setup_on_mainet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eux
-
-source tools/deployment/store_instantiate.sh "tools/deployment/config_mainet.json" false
-source tools/deployment/populate_contracts.sh "tools/deployment/config_mainet.json"
