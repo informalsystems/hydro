63735c5c6474122663fd1c4c80d3a43c680b9e4b
Philip Offtermatt <p.offtermatt@gmail.com>
2024-12-05 14:33:30 +0100

    Add script for testnet deployment

---
+++ tools/deployment/setup_on_testnet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -eux
+
+source tools/deployment/store_instantiate.sh "tools/deployment/config_testnet.json" false
+source tools/deployment/populate_contracts.sh "tools/deployment/config_testnet.json"
