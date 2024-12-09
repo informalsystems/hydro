443e1db9857fee2f1befc31a54933382cab20d5e
Dusan Maksimovic <dusan.maksimovic@ethernal.tech>
2024-12-09 14:11:47 +0100

    remove old migration files

--- contracts/tribute/src/migration/mod.rs
+++ contracts/tribute/src/migration/mod.rs
@@ -1,5 +1,2 @@
 pub mod migrate;
-pub mod v1_1_0;
-pub mod v1_1_1;
-pub mod v2_0_1;
 pub mod v2_0_4;
--- contracts/tribute/src/migration/v1_1_0.rs
+++
@@ -1,8 +0,0 @@
-use cosmwasm_std::Uint128;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct MigrateMsgV1_1_0 {
-    pub min_prop_percent_for_claimable_tributes: Uint128,
-}
