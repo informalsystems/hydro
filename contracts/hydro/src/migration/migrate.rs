3c32126ce2e4eec00ce6b211d49fb77014f12611
Dusan Maksimovic <dusan.maksimovic@ethernal.tech>
2024-12-09 13:01:55 +0100

    - remove obsolete migration code - bump contracts version to v2.0.4 - generate changelog for new version - recompile contracts, regenerate schema

--- contracts/hydro/src/migration/migrate.rs
+++ contracts/hydro/src/migration/migrate.rs
@@ -7,7 +7,7 @@ use cw2::{get_contract_version, set_contract_version};
 use neutron_sdk::bindings::msg::NeutronMsg;
 use neutron_sdk::bindings::query::NeutronQuery;
 
-use super::v2_0_1::{migrate_v1_1_0_to_v2_0_1, MigrateMsgV2_0_1};
+use super::v2_0_4::MigrateMsgV2_0_4;
 
 pub const CONTRACT_VERSION_V1_1_0: &str = "1.1.0";
 pub const CONTRACT_VERSION_V2_0_1: &str = "2.0.1";
@@ -15,9 +15,9 @@ pub const CONTRACT_VERSION_V2_0_2: &str = "2.0.2";
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(
-    mut deps: DepsMut<NeutronQuery>,
+    deps: DepsMut<NeutronQuery>,
     _env: Env,
-    msg: MigrateMsgV2_0_1,
+    _msg: MigrateMsgV2_0_4,
 ) -> Result<Response<NeutronMsg>, ContractError> {
     let contract_version = get_contract_version(deps.storage)?;
 
@@ -25,10 +25,6 @@ pub fn migrate(
         return Err(ContractError::Std(StdError::generic_err(
             "Contract is already migrated to the newest version.",
         )));
-    }
-
-    if contract_version.version == CONTRACT_VERSION_V1_1_0 {
-        migrate_v1_1_0_to_v2_0_1(&mut deps, msg)?;
     }
 
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
