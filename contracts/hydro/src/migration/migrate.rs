d18193ef29db4246309d61886a07bd59f7357f04
Philip Offtermatt <p.offtermatt@gmail.com>
2025-02-26 11:20:08 +0100

    Fix imports for migration message

--- contracts/hydro/src/migration/migrate.rs
+++ contracts/hydro/src/migration/migrate.rs
@@ -6,6 +6,8 @@ use cosmwasm_std::{entry_point, DepsMut, Env, Response, StdError};
 use cw2::{get_contract_version, set_contract_version};
 use neutron_sdk::bindings::msg::NeutronMsg;
 use neutron_sdk::bindings::query::NeutronQuery;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
 pub const CONTRACT_VERSION_V1_1_0: &str = "1.1.0";
 pub const CONTRACT_VERSION_V2_0_1: &str = "2.0.1";
@@ -14,6 +16,7 @@ pub const CONTRACT_VERSION_V2_1_0: &str = "2.1.0";
 pub const CONTRACT_VERSION_V3_0_0: &str = "3.0.0";
 pub const CONTRACT_VERSION_V3_1_0: &str = "3.1.0";
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct MigrateMsgV3_1_1 {}
 
 #[cfg_attr(not(feature = "library"), entry_point)]
