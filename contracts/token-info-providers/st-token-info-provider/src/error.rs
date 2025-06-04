d77af8c774317eac5aee4fdadf74fd6ca80c4f48
Dusan Maksimovic <dusan.maksimovic@ethernal.tech>
2025-06-04 15:44:36 +0200

    stTOKEN ratio update implementation

--- contracts/token-info-providers/st-token-info-provider/src/error.rs
+++ contracts/token-info-providers/st-token-info-provider/src/error.rs
@@ -1,8 +1,15 @@
 use cosmwasm_std::StdError;
+use neutron_sdk::NeutronError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("{0}")]
+    NeutronError(#[from] NeutronError),
+
+    #[error("Unauthorized")]
+    Unauthorized,
 }
--- contracts/token-info-providers/st-token-info-provider/src/query.rs
+++ contracts/token-info-providers/st-token-info-provider/src/query.rs
@@ -1,6 +1,7 @@
-use crate::state::Config;
+use crate::state::{Config, InterchainQueryInfo};
 use cosmwasm_schema::{cw_serde, QueryResponses};
 
+use cosmwasm_std::Timestamp;
 // When compiling for wasm32 platform, compiler doesn't recognize that this type is used in one of the queries.
 #[allow(unused_imports)]
 use interface::token_info_provider::DenomInfoResponse;
@@ -11,6 +12,9 @@ pub enum QueryMsg {
     #[returns(ConfigResponse)]
     Config {},
 
+    #[returns(InterchainQueryInfoResponse)]
+    InterchainQueryInfo {},
+
     #[returns(DenomInfoResponse)]
     DenomInfo { round_id: u64 },
 }
@@ -18,4 +22,24 @@ pub enum QueryMsg {
 #[cw_serde]
 pub struct ConfigResponse {
     pub config: Config,
+}
+
+#[cw_serde]
+pub struct InterchainQueryInfoResponse {
+    pub info: Option<InterchainQueryInfo>,
+}
+
+// TODO: The following two data structures should be replaced with the ones from the interface package once the
+// LSM integration PR that introduces them gets merged.
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum HydroQueryMsg {
+    #[returns(HydroCurrentRoundResponse)]
+    CurrentRound {},
+}
+
+#[cw_serde]
+pub struct HydroCurrentRoundResponse {
+    pub round_id: u64,
+    pub round_end: Timestamp,
 }
