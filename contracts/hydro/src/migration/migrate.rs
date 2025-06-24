e9557fd1a49ffb2da3f993c83ae7aecffa648cc9
Dusan Maksimovic <dusan.maksimovic@ethernal.tech>
2025-06-24 11:16:38 +0200

    fixed v3.4.0 migration

--- contracts/hydro/src/migration/migrate.rs
+++ contracts/hydro/src/migration/migrate.rs
@@ -3,13 +3,14 @@ use crate::error::{new_generic_error, ContractError};
 use crate::state::CONSTANTS;
 // entry_point is being used but for some reason clippy doesn't see that, hence the allow attribute here
 use crate::utils::load_constants_active_at_timestamp;
+use cosmwasm_schema::cw_serde;
 #[allow(unused_imports)]
 use cosmwasm_std::{entry_point, DepsMut, Env, Response, StdError};
 use cw2::{get_contract_version, set_contract_version};
 use neutron_sdk::bindings::msg::NeutronMsg;
 use neutron_sdk::bindings::query::NeutronQuery;
 
-use super::v3_3_0::{migrate_v3_3_0_to_v3_4_0, MigrateMsgV3_3_0};
+use super::v3_4_1::migrate_v3_2_0_to_v3_4_1;
 
 pub const CONTRACT_VERSION_V1_1_0: &str = "1.1.0";
 pub const CONTRACT_VERSION_V2_0_1: &str = "2.0.1";
@@ -18,18 +19,21 @@ pub const CONTRACT_VERSION_V2_1_0: &str = "2.1.0";
 pub const CONTRACT_VERSION_V3_0_0: &str = "3.0.0";
 pub const CONTRACT_VERSION_V3_1_0: &str = "3.1.0";
 pub const CONTRACT_VERSION_V3_1_1: &str = "3.1.1";
-pub const CONTRACT_VERSION_V3_2_1: &str = "3.2.1";
-pub const CONTRACT_VERSION_V3_3_0: &str = "3.3.0";
+pub const CONTRACT_VERSION_V3_2_0: &str = "3.2.0";
+pub const CONTRACT_VERSION_V3_4_1: &str = "3.4.1";
+
+#[cw_serde]
+pub struct MigrateMsg {}
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(
     mut deps: DepsMut<NeutronQuery>,
     _env: Env,
-    _msg: MigrateMsgV3_3_0,
+    _msg: MigrateMsg,
 ) -> Result<Response<NeutronMsg>, ContractError> {
-    check_contract_version(deps.storage, CONTRACT_VERSION_V3_3_0)?;
+    check_contract_version(deps.storage, CONTRACT_VERSION_V3_2_0)?;
 
-    let response = migrate_v3_3_0_to_v3_4_0(&mut deps)?;
+    let response = migrate_v3_2_0_to_v3_4_1(&mut deps)?;
 
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
