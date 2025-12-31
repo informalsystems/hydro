c2c616c68d38f3fb562230928b860a737b9c935c
stana.miric <stana.miric@ethernal.tech>
2025-12-31 16:12:13 +0100

    Implement dAtom redemption adapter

---
+++ contracts/inflow/datom-adapter/src/msg.rs
@@ -0,0 +1,53 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+pub use interface::inflow_adapter::{AdapterInterfaceMsg, AdapterInterfaceQueryMsg};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub admins: Vec<String>,
+    pub executors: Vec<String>,
+
+    pub drop_staking_core: String,
+    pub drop_voucher: String,
+    pub drop_withdrawal_manager: String,
+
+    pub vault_contract: String,
+
+    pub datom_denom: String,
+    pub atom_denom: String,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    /// Adapter interface entrypoint
+    StandardAction(AdapterInterfaceMsg),
+
+    /// Datom-specific logic
+    CustomAction(DatomAdapterMsg),
+}
+
+#[cw_serde]
+pub enum DatomAdapterMsg {
+    /// executor-only
+    Unbond {},
+
+    /// executor-only
+    Withdraw { token_id: String },
+
+    /// admin-only
+    UpdateConfig {
+        drop_staking_core: Option<String>,
+        drop_voucher: Option<String>,
+        drop_withdrawal_manager: Option<String>,
+        vault_contract: Option<String>,
+    },
+
+    /// admin-only
+    UpdateExecutors { executors: Vec<String> },
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    #[returns(cosmwasm_std::Binary)]
+    StandardQuery(AdapterInterfaceQueryMsg),
+}
